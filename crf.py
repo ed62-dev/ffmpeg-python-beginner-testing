92c5b7ad0c4acc0dcdeef6df48f0068cda43d2ea
ed62-dev <59343410+ed62-dev@users.noreply.github.com>
2024-02-07 15:38:22 +0200

    crf.py

--- crf.py
+++ crf.py
@@ -15,15 +15,20 @@ input_file = filedialog.askopenfilename(title="Select a movie file")
 window.deiconify()
 
 # Prompt the user to enter the desired CRF value
-crf_value = input("Enter CRF value (default is 28): ")
+'''crf_value = input("Enter CRF value (default is 28): ")
 if not crf_value:
-    crf_value = "28"
+    crf_value = "28"'''
+
+# Get the new file name, mkv preffered
+output_file = filedialog.asksaveasfilename(defaultextension='.mkv')
 
 # Specify the output file name
-output_file = "output.mp4"
+#output_file = "output.mp4"
 
 # Construct the ffmpeg command
-command = f"ffmpeg -i {input_file} -vcodec libx264 -crf {crf_value} {output_file}"
+#command = f"ffmpeg -i {input_file} -vcodec libx264 -crf {crf_value} {output_file}"
+
+command = f"ffmpeg -i {input_file} -vcodec libx264 -crf 28 {output_file}"
 
 # Execute the ffmpeg command
 subprocess.call(command, shell=True)
